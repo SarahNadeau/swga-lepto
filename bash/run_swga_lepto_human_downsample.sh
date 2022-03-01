d4ba4027e9237618288d36d03b64edf7dde571b3
Sarah Nadeau <30396464+SarahNadeau@users.noreply.github.com>
2022-03-01 10:05:28 -0500

    Fix directory name

--- bash/run_swga_lepto_human_downsample.sh
+++ bash/run_swga_lepto_human_downsample.sh
@@ -8,13 +8,13 @@ echo "$N_LINES chunks of lenth $CHUNK_SIZE used"
 
 module load nextflow
 
-mkdir -p $HOME/lepto_swga/swga_results
+mkdir -p $HOME/swga_lepto/swga_results
 
 nextflow run \
 	-profile singularity $HOME/wf-swga/main.nf \
-	--outpath $HOME/lepto_swga/swga_results/lepto_human_downsample${N_LINES} \
-	--target $HOME/lepto_swga/target_lepto.fasta \
-	--background $HOME/lepto_swga/background_human.fasta \
+	--outpath $HOME/swga_lepto/swga_results/lepto_human_downsample${N_LINES} \
+	--target $HOME/swga_lepto/target_lepto.fasta \
+	--background $HOME/swga_lepto/background_human.fasta \
 	--target-chunk-size $CHUNK_SIZE \
 	--backgr-chunk-size $CHUNK_SIZE \
 	--target-n-chunks $N_LINES \
