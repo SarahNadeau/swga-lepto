f81486d134d9a50f764b269050d3aee6b619d9c0
SarahNadeau <san62@cornell.edu>
2022-04-06 09:44:40 +0200

    submit one job per secondary target genome to reduce load on cluster

--- bash/_score_sets_on_secondary_targets.sh
+++ bash/_score_sets_on_secondary_targets.sh
@@ -2,45 +2,53 @@
 
 # This script is to run the SWGA software to score a primer set on a secondary target genome.
 
-PRIMER_FILE=$1
-TARGET_GENOME=$2
-OUTFILE=$3
+TARGET_GENOME=$1
+WORKDIR=$2
 
-# Remove any blank lines in target
-TMPDIR=swga_tmpdir_$RANDOM
-echo "TMPDIR: $TMPDIR"
-mkdir -p $TMPDIR && cd $TMPDIR
+for PRIMER_FILE in $WORKDIR/set_to_score_size_*_idx_*_primers.txt; do
 
-gunzip -c $TARGET_GENOME > target.fasta
-awk NF target.fasta >> target.fasta.noblanks
+  # Define job-specific outfile
+  TARGET_PREFIX="$(basename $TARGET_GENOME .fna.gz)"
+  PRIMER_PREFIX="$(basename $PRIMER_FILE _primers.txt)"
+  OUTFILE=$WORKDIR/${PRIMER_PREFIX}__${TARGET_PREFIX}.txt
 
-# We don't care about background frequency (already calculated) and don't want to waste time counting more k-mers
-echo ">dummy background genome
-CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC" > background.fasta
+  # Remove any blank lines in target
+  TMPDIR=swga_tmpdir_$RANDOM
+  echo "TMPDIR: $TMPDIR"
+  mkdir -p $TMPDIR && cd $TMPDIR
 
-# No exclusionary sequences
-echo ">empty exclude sequence" > exclude.fasta
+  gunzip -c $TARGET_GENOME > target.fasta
+  awk NF target.fasta >> target.fasta.noblanks
 
-swga init \
-  -f target.fasta.noblanks \
-  -b background.fasta \
-  -e exclude.fasta
+  # We don't care about background frequency (already calculated) and don't want to waste time counting more k-mers
+  echo ">dummy background genome
+  CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC" > background.fasta
 
-# Count kmers
-swga count
+  # No exclusionary sequences
+  echo ">empty exclude sequence" > exclude.fasta
 
-# Activate the primers and score them as a set
-swga activate \
-  --input $PRIMER_FILE
-swga score \
-  --input $PRIMER_FILE \
-  --force  # don't prompt to add to database
+  swga init \
+    -f target.fasta.noblanks \
+    -b background.fasta \
+    -e exclude.fasta
 
-# By default the first manually added set has ID -1
-swga export sets --id -1 >> $OUTFILE
+  # Count kmers
+  swga count
 
-cd ../
+  # Activate the primers and score them as a set
+  swga activate \
+    --input $PRIMER_FILE
+  swga score \
+    --input $PRIMER_FILE \
+    --force  # don't prompt to add to database
 
+  # By default the first manually added set has ID -1
+  swga export sets --id -1 >> $OUTFILE
+
+  cd ../
+  rm -r $TMPDIR
+
+done
 
 
 
--- bash/score_sets_on_secondary_targets.sh
+++ bash/score_sets_on_secondary_targets.sh
@@ -9,19 +9,12 @@ export SINGULARITY_CACHEDIR=/scicomp/scratch/$USER/singularity.cache
 
 # Iterate through other Lepto species genomes, score set on each one
 for TARGET_GENOME in $HOME/swga-lepto/lepto_species_assemblies/ncbi-genomes-2022-03-09/*.fna.gz; do
-  for PRIMER_FILE in $WORKDIR/set_to_score_size_*_idx_*_primers.txt; do
 
-    # Define job-specific outfile
-    TARGET_PREFIX="$(basename $TARGET_GENOME .fna.gz)"
-    PRIMER_PREFIX="$(basename $PRIMER_FILE _primers.txt)"
-    OUTFILE=$WORKDIR/${PRIMER_PREFIX}__${TARGET_PREFIX}.txt
+  # Submit job
+  qsub \
+    -N score_sets \
+    $HOME/swga-lepto/bash/_qsub_run_swga_lepto_human_like_nau.sh $TARGET_GENOME $WORKDIR
 
-    # Submit job
-    qsub \
-      -N score_sets \
-      $HOME/swga-lepto/bash/_qsub_run_swga_lepto_human_like_nau.sh $PRIMER_FILE $TARGET_GENOME $OUTFILE
-
-  done
 done
 
 
