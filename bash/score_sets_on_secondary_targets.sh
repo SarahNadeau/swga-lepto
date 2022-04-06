1d7cf346577b2171609b98db7deab585d273000b
SarahNadeau <san62@cornell.edu>
2022-04-06 14:41:31 +0200

    take workdir as input to score_sets_on_secondary_targets.sh

--- bash/score_sets_on_secondary_targets.sh
+++ bash/score_sets_on_secondary_targets.sh
@@ -2,7 +2,7 @@
 
 # This wrapper script is to call a script to run the SWGA software to score primer sets on secondary target genomes.
 
-WORKDIR=$HOME/swga-lepto/swga_results/like_nau_try_2
+WORKDIR=$1
 
 # Iterate through other Lepto species genomes, score set on each one
 for TARGET_GENOME in $HOME/swga-lepto/lepto_species_assemblies/ncbi-genomes-2022-03-09/*.fna.gz; do
